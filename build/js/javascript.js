119f5baf380339969004c5c2347090613a39987f
Ol-Lis <olegnu16@gmail.com>
2024-08-13 09:25:29 +0300

    try ti fix not work style tailwind

--- build/js/javascript.js
+++ build/js/javascript.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
-<<<<<<< HEAD
-  $('.slider').slick({
-=======
   $(".slider").slick({
->>>>>>> 2ad0e8f8729414ecf5ca9f411cd5da9d34181422
     autoplay: true,
     autoplaySpeed: 1500,
     infinity: true,
