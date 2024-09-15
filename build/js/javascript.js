033e32864371ab25e7046e0713f97cb43a007ce0
Dmytro Shynkarenko <dmytroshynkarenko26@gmail.com>
2024-09-15 21:05:34 +0300

    make a componets footer header

--- build/js/javascript.js
+++ build/js/javascript.js
@@ -9,3 +9,18 @@ $(document).ready(function () {
     dots: true,
   });
 });
+
+function loadComponent(url, containerId) {
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(containerId).innerHTML = data;
+    })
+    .catch((error) => console.error("Помилка завантаження компонента:", error));
+}
+
+// Завантаження хедера
+loadComponent("header.html", "header-container");
+
+// Завантаження футера
+loadComponent("footer.html", "footer-container");
