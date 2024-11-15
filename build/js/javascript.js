773d09bb9d30e357420bf6537ec24653c922ba6d
Ol-Lis <olegnu16@gmail.com>
2024-11-15 12:39:18 +0200

    plus time on slider

--- build/js/javascript.js
+++ build/js/javascript.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(".slider").slick({
     autoplay: true,
-    autoplaySpeed: 1500,
+    autoplaySpeed: 3000,
     infinity: true,
     speed: 1000,
     // adaptiveHeight: true,
